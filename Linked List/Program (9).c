a87b48a0309bda0f3ba7d8e9374c8a4f47af5e07
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-07 23:45:06 +0530

    Add files via upload

---
+++ Linked List/Program (9).c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+//Callbyvalue
+
+void Display(PNODE first)
+{}
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+//CallByAddress
+
+void InserFirst(PPNODE first, int no)
+{
+
+}
+
+void InsertLast(PPNODE first, int no)
+{
+
+}
+
+void InsertAtPos(PPNODE first,int no, int pos)
+{
+
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+
+    Display(head);
+    Count(head);
+
+    InserFirst(&head,11);
+    InsertLast(&head,21);
+    InsertAtPos(&head,51,5);
+
+    return 0;
+}
