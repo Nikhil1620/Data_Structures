aae536135e54a2927d25607de24197c8f0f5bf16
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-09 23:12:17 +0530

    Add files via upload

---
+++ Linked List/Program_(16).c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;\
+
+
+void InserFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if((*first) == NULL)   //Linked list is empty
+    {
+        (*first) = newn;
+    }
+    else                 //Linked list contains atleast one node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+}
+
+void InserLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*first == NULL)   //Linked list is empty
+    {
+        *first = newn;
+    }
+    else                 //Linked list contains atleast one node
+    {
+        
+    }
+
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE head = NULL;
+
+    InserFirst(&head,75);
+    InserFirst(&head,51);
+    InserFirst(&head,21);
+    InserFirst(&head,11);
+
+    Display(head);
+
+    iRet = Count(head);
+    printf("\n");
+    printf("%d nodes are in the linked list.",iRet);
+    printf("\n");
+
+    return 0;
+}
