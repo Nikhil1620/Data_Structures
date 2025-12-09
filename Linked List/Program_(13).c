dd88d9604dca9462a86ce004c8f068e63f83f83c
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-09 23:10:17 +0530

    Add files via upload

---
+++ Linked List/Program_(13).c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;\
+
+
+void InserFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if((*first) == NULL)   //Linked list is empty
+    {
+        (*first) = newn;
+    }
+    else                 //Linked list contains atleast one node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+}
+
+void InserLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*first == NULL)   //Linked list is empty
+    {
+        *first = newn;
+    }
+    else                 //Linked list contains atleast one node
+    {
+        
+    }
+
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InserFirst(&head,51);
+    InserFirst(&head,21);
+    InserFirst(&head,11);
+
+    return 0;
+}
