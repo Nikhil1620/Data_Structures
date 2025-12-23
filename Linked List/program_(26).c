c46b0582de771c40667292ce1640105b2e778b56
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-23 10:36:05 +0530

    Add files via upload

---
+++ Linked List/program_(26).c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;   //$
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InserFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data =no;
+    newn -> next = NULL;
+    newn -> prev = NULL;    //$
+
+    if((*first) == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn -> next = (*first);
+        (*first) -> prev = newn;
+        (*first) = newn;
+    }
+}
+
+void InserLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data =no;
+    newn -> next = NULL;
+    newn -> prev = NULL;    //$
+
+    if((*first) == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+
+    }
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+
+}
+
+void DeleteFirst(PPNODE first)
+{
+
+}
+
+void DeleteLast(PPNODE first)
+{
+
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+    
+}
+
+void Display(PNODE first)
+{
+    printf("\nNULL");
+    while(first != NULL)
+    {
+        printf("| %d | <=> ",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InserFirst(&head,51);
+    InserFirst(&head,21);
+    InserFirst(&head,11);
+    Display(head);
+
+    return 0;
+}
