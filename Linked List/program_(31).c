f663a4ad0219b9c13d1934a749c1920624446dd9
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-23 10:37:46 +0530

    Add files via upload

---
+++ Linked List/program_(31).c
@@ -0,0 +1,179 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;   //$
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InserFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data =no;
+    newn -> next = NULL;
+    newn -> prev = NULL;    //$
+
+    if((*first) == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn -> next = (*first);
+        (*first) -> prev = newn;  //$
+        (*first) = newn;
+    }
+}
+
+void InserLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data =no;
+    newn -> next = NULL;
+    newn -> prev = NULL;    //$
+
+    if((*first) == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        temp = (*first);
+        
+        while(temp->next != NULL)         //Type 2 while loop
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = newn;
+        temp->prev = temp;           //$
+    }
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+    else if((*first) -> next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        temp = *first;
+
+        *first = (*first) -> next;
+        free(temp);
+        (*first) -> prev = NULL;        //$
+    }
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+    else if((*first) -> next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp ->next -> next != NULL)      // Type 3
+        {
+            temp = temp -> next;
+        }
+
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+    
+}
+
+void Display(PNODE first)
+{
+    printf("\nNULL");
+    while(first != NULL)
+    {
+        printf("| %d | <=> ",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+     int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InserFirst(&head,51);
+    InserFirst(&head,21);
+    InserFirst(&head,11);
+    Display(head);
+    
+    iRet = Count(head);
+    printf("\n");
+    printf("%d nodes are in the linked list.",iRet);
+    printf("\n");
+
+    InserLast(&head,101);
+    InserLast(&head,111);
+    InserLast(&head,121);
+    
+    Display(head);
+    
+    iRet = Count(head);
+    printf("\n");
+    printf("%d nodes are in the linked list.",iRet);
+    printf("\n");
+
+
+    return 0;
+}
