1d8f7ecda0affa300bf6ce9d96b7097273b3807f
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-23 10:38:43 +0530

    Add files via upload

---
+++ Linked List/program_(34).c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InserFirst(PPNODE first, PPNODE last, int no)
+{
+
+}
+
+void InserLast(PPNODE first, PPNODE last, int no)
+{
+
+}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int pos)
+{
+
+}
+
+void DeleteFirst(PNODE first, PNODE last)
+{
+
+}
+
+void DeleteLast(PNODE first, PNODE last)
+{
+
+}
+
+void DeleteAtPos(PNODE first, PNODE last, int pos)
+{
+
+}
+
+void Display(PNODE first, PNODE last)
+{
+
+}
+
+int Count(PNODE first, PNODE last)
+{
+    return 0;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;    //@
+
+    return 0;
+}
