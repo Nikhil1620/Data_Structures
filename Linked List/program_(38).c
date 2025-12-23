86205e009f655aa84c526d855a4a022d36a28a00
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-23 10:40:15 +0530

    Add files via upload

---
+++ Linked List/program_(38).c
@@ -0,0 +1,127 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(((*first) == NULL) && ((*last) == NULL))
+    {
+        (*first) = newn;
+        (*last) = newn;
+    }
+    else
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    (*last) -> next = (*first);             //@
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(((*first) == NULL) && ((*last) == NULL))
+    {
+        (*first) = newn;
+        (*last) = newn;
+    }
+    else
+    {
+        (*last) -> next = newn;
+        (*last) = newn;
+    }
+    (*last) -> next = (*first);                   //@
+}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int pos)
+{
+
+}
+
+void DeleteFirst(PNODE first, PNODE last)
+{
+
+}
+
+void DeleteLast(PNODE first, PNODE last)
+{
+
+}
+
+void DeleteAtPos(PNODE first, PNODE last, int pos)
+{
+
+}
+
+void Display(PNODE first, PNODE last)
+{
+    do
+    {
+        printf("| %d | -> ", first -> data);
+        first = first -> next;
+
+    }while(first != last -> next);
+}
+
+int Count(PNODE first, PNODE last)
+{
+    int iCount = 0;
+
+    do
+    {
+        iCount++;
+        first = first -> next;
+
+    }while(first != last -> next);
+
+    return iCount;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE head = NULL;
+    PNODE tail = NULL;    //@
+
+    InsertFirst(&head,&tail,51);
+    InsertFirst(&head,&tail,21);
+    InsertFirst(&head,&tail,11);
+
+    InsertLast(&head,&tail,101);
+    InsertLast(&head,&tail,111);
+    InsertLast(&head,&tail,121);
+
+    Display(head,tail);
+
+    iRet = Count(head,tail);
+    printf("\n");
+    printf("%d nodes are in the linked list.",iRet);
+    printf("\n");
+
+    
+
+    return 0;
+}
